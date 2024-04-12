f0e852e66c54df7d945d6d3286a78660d438c36f
Pritirekha panda <learnpriti23@gmail.com>
2024-04-12 23:02:44 +0530

    added two buttons in card

--- src/pages/Components/AirCopcoVT4Parts/Aircopcovt4parts.js
+++ src/pages/Components/AirCopcoVT4Parts/Aircopcovt4parts.js
@@ -7,19 +7,22 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActionArea from '@mui/material/CardMedia';
 import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import ShareIcon from '@mui/icons-material/Share';
 
 
 const cardData = [
   {
     image: 'https://static.wixstatic.com/media/f16a02_0f8f3febcd9a48319fc12695180c72d3~mv2.jpg/v1/fill/w_483,h_342,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/f16a02_0f8f3febcd9a48319fc12695180c72d3~mv2.jpg',
-    alt:   '  Stud 1404124800 Replacement parts',
+    alt: '  Stud 1404124800 Replacement parts',
     heading: '  Stud 1404124800 Replacement parts',
 
   },
   {
     image: 'https://static.wixstatic.com/media/f16a02_66aebf78a3f643af8dae91b8fe0b760a~mv2.jpg/v1/fill/w_483,h_342,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/f16a02_66aebf78a3f643af8dae91b8fe0b760a~mv2.jpg',
-    alt:      ' Head HP 1614030302 Replacement',
-    heading:  '  Head HP 1614030302 Replacement',
+    alt: ' Head HP 1614030302 Replacement',
+    heading: '  Head HP 1614030302 Replacement',
   },
   {
 
@@ -30,13 +33,13 @@ const cardData = [
 
   {
     image: "https://static.wixstatic.com/media/f16a02_c66b6370fff2467f8ac29d73c00e9bde~mv2.jpg/v1/fill/w_483,h_342,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/f16a02_c66b6370fff2467f8ac29d73c00e9bde~mv2.jpg",
-    alt:     " Circlip 0335213200 Replacement parts",
+    alt: " Circlip 0335213200 Replacement parts",
     heading: '   Circlip 0335213200 Replacement parts',
 
   },
   {
     image: "https://static.wixstatic.com/media/d7032a_b870dd93e8f641aca875a71eb67cbf5f~mv2.jpg/v1/fill/w_483,h_342,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d7032a_b870dd93e8f641aca875a71eb67cbf5f~mv2.jpg",
-    alt:     "Guard, Valve 1613119100 Replacement",
+    alt: "Guard, Valve 1613119100 Replacement",
     heading: "Guard, Valve 1613119100 Replacement",
 
   },
@@ -48,26 +51,26 @@ const cardData = [
   },
   {
     image: "https://static.wixstatic.com/media/d7032a_c22fe4a4b172454b8ef25dd2ad4e9e06~mv2.jpg/v1/fill/w_483,h_342,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d7032a_c22fe4a4b172454b8ef25dd2ad4e9e06~mv2.jpg",
-    alt:  ' Drum, Fan, Lower 1613013800 Replacement   ',
+    alt: ' Drum, Fan, Lower 1613013800 Replacement   ',
     heading: " Drum, Fan, Lower 1613013800 Replacement ",
 
   },
-  
+
   {
     image: "https://static.wixstatic.com/media/d7032a_c22fe4a4b172454b8ef25dd2ad4e9e06~mv2.jpg/v1/fill/w_483,h_342,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d7032a_c22fe4a4b172454b8ef25dd2ad4e9e06~mv2.jpg",
-    alt:     ' Hourmeter, Compl. 9718253700 Replacement ',
+    alt: ' Hourmeter, Compl. 9718253700 Replacement ',
     heading: "Hourmeter, Compl. 9718253700 Replacement ",
 
   },
   {
     image: "https://static.wixstatic.com/media/d7032a_c22fe4a4b172454b8ef25dd2ad4e9e06~mv2.jpg/v1/fill/w_483,h_342,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d7032a_c22fe4a4b172454b8ef25dd2ad4e9e06~mv2.jpg",
-    alt:     ' Coupling, Compl. 1020261600 Replacement  ',
+    alt: ' Coupling, Compl. 1020261600 Replacement  ',
     heading: " Coupling, Compl. 1020261600 Replacement ",
 
   },
   {
     image: "https://static.wixstatic.com/media/d7032a_c22fe4a4b172454b8ef25dd2ad4e9e06~mv2.jpg/v1/fill/w_483,h_342,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d7032a_c22fe4a4b172454b8ef25dd2ad4e9e06~mv2.jpg",
-    alt:     '  Nut 0266110800 Replacement parts ',
+    alt: '  Nut 0266110800 Replacement parts ',
     heading: "  Nut 0266110800 Replacement parts",
 
   },
@@ -86,7 +89,7 @@ const cardData = [
 ];
 
 
-const Aircopcovt4parts= () => {
+const Aircopcovt4parts = () => {
   return (
 
     <Grid className="home-container-page">
@@ -94,7 +97,7 @@ const Aircopcovt4parts= () => {
       <Grid item xs={12} className="home-page-textf">
         <h1><u>Atlas Copco VT 4 Parts</u></h1>
         <Grid item xs={12} md={6} className="mt-5">
-        Atlas Copco VT 4 Air Compressor Parts - Atlas Copco Compressor Parts - Air Compressor Spare Parts.
+          Atlas Copco VT 4 Air Compressor Parts - Atlas Copco Compressor Parts - Air Compressor Spare Parts.
         </Grid>
 
         <Grid item xs={12} md={6} className="mt-4">PFC - parts for compressor is a Global Supplier of OEM quality new aftermarket replacement air compressor spare parts - Atlas Copco VT 4 Air Compressor Parts , Atlas Copco VT 5 Air Compressor Parts - Atlas Copco Compressor Parts from India.</Grid>
@@ -113,13 +116,18 @@ const Aircopcovt4parts= () => {
                   style={{ height: '150' }} // Adjust image height and width of the card here 
                 /><hr />
 
-                <CardContent className=' p-3 bg-info text-dark '  style={{ borderRadius: '40px 0px 40px 0px ' }}>
+                <CardContent className=' p-3 bg-info text-dark ' >
                   <Typography variant="h5" component="div" className='home-page-image-heading'>
                     {card.heading}
                   </Typography>
                 </CardContent>
-
               </CardActionArea>
+              <CardActions>
+                <a href="/ContactUs" style={{ textDecoration: 'none' }}>
+                  <Button  variant="contained"  color="success" >Contact Us</Button>
+                </a>
+                <Button  variant="contained"  color="error" className="btn btn-success" href="./assets/01_1203CompressorFrameAssembly-Model7T2.pdf">Download PDF</Button>     <ShareIcon />
+              </CardActions>
             </Card>
           </Grid>
         ))}
