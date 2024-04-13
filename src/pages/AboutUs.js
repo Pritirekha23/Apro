eb44e8ab1e26d79962ee14dca52b6a8d52e81d35
Pritirekha panda <learnpriti23@gmail.com>
2024-04-13 16:56:36 +0530

    added aboutus final review service part

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,6 +4,7 @@ import './style/AboutUs.css';
 import abtusimg1 from './assets/abtusimg1.jpeg';
 import animation_cartoon from './assets/animation_cartoon.png'
 import SCP from './assets/SCP.gif';
+import oursevicess from './assets/oursevicess.jpg';
 
 const AboutUS = () => {
   return (
@@ -34,14 +35,15 @@ const AboutUS = () => {
         <Grid item xs={12} sm={6} className='abtus-grid-2-img' >
           <img src={abtusimg1} className="abtus-grid-2-image animate__animated animate__fadeInRight" alt="this is first image of about us page" />
         </Grid>
-      </Grid><br /> <br />  <br />
+      </Grid><br /><br /><br />
 
 
       <Grid className='productandservices'>
-        <Grid item xs={12} sm={6} className='abtus-grid-2-img' >
+        <Grid item xs={12} sm={6} className='abtus-grid-2-img hide-on-mobile b' >
           <img src={SCP} className="ab" alt="this is first image of about us page" style={{
             width: "600px",
-            height: "120%"
+            height: "120%",
+            borderRadius: "2%"
           }} />
         </Grid>
         <Grid >
@@ -55,16 +57,27 @@ const AboutUS = () => {
       </Grid>
 
       <br /> <br />  <br />
-      <Grid>
-        <Typography className='aboutus-page-heading '> <b>  <u>SER</u>VICES</b></Typography><br />
-        You would definitely agree that for better & consistent supply of Air Power, servicing of the compressors should be of high quality. With this in mind we have a dedicated, qualified and highly experienced group of Engineers who would attend to your problems / break downs round-the-clock.
 
-        On entering into an Annual Maintenance Contract with us you can be absolutely sure of trouble-free compressor operation throughout the contract period. In fact, we help you in achieving zero-break down for compressors throughout the year. Our team of service Engineers are available to you round-the-clock to attend to break down calls. As a further proof of our dedication we are in a position to carryout the maintenance of your Compressors on Sundays and National Holidays without affecting your production schedule. You also save a lot of expenditure by not employing people separately for the purpose of Compressor Maintenance and our AMC rates are heard to be believed.
+      <Grid class="onlyservices hide-on-mobile">
 
-        If you judge a Company by the clients it has or the business relations it keeps, you would agree that we are far above our competitors. Year after year we are able to meet the exacting standards set by the leading MNCs, PSUs & Indian Companies in different Industries.
+        <Grid>
+          <Typography className='aboutus-page-heading '> <b>  <u>SER</u>VICES</b></Typography><br />
+          You would definitely agree that for better & consistent supply of Air Power, servicing of the compressors should be of high quality. With this in mind we have a dedicated, qualified and highly experienced group of Engineers who would attend to your problems / break downs round-the-clock.
+
+          On entering into an Annual Maintenance Contract with us you can be absolutely sure of trouble-free compressor operation throughout the contract period. In fact, we help you in achieving zero-break down for compressors throughout the year. Our team of service Engineers are available to you round-the-clock to attend to break down calls. As a further proof of our dedication we are in a position to carryout the maintenance of your Compressors on Sundays and National Holidays without affecting your production schedule. You also save a lot of expenditure by not employing people separately for the purpose of Compressor Maintenance and our AMC rates are heard to be believed.
+
+          If you judge a Company by the clients it has or the business relations it keeps, you would agree that we are far above our competitors. Year after year we are able to meet the exacting standards set by the leading MNCs, PSUs & Indian Companies in different Industries.
 
 
-        We, at Airpro, believe in and look forward to establishing long-term, mutually beneficial business relationships. Business with us means entering the realm of professionalism and leaving the ordinary behind. Kindly get in touch with Mr. Manas & Debasish to know more about our services. We would very much appreciate your kind gesture of letting us visit your premises to offer personalized service / solutions.
+          We, at Airpro, believe in and look forward to establishing long-term, mutually beneficial business relationships. Business with us means entering the realm of professionalism and leaving the ordinary behind. Kindly get in touch with Mr. Manas & Debasish to know more about our services. We would very much appreciate your kind gesture of letting us visit your premises to offer personalized service / solutions.
+        </Grid>
+        <Grid item xs={12} sm={6} className='abtus-grid-2-img' >
+          <img src={oursevicess} className='b animate__animated animate__bounce' alt="this is first image of about us page" style={{
+            width: "600px",
+            height: "100%",
+            borderRadius: "2%"
+          }} />
+        </Grid>
 
       </Grid>
     </Grid>
